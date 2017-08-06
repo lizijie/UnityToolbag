cef09c9c2984933bc9858f40e8260b3ab2c99b3b
zijieli <425880365@qq.com>
2017-08-06 16:04:17 +0800

    扩展Animation动画Inspector窗口
    列表显示Animation的所有动画。并且可选择播放

---
+++ Animation/AnimationShower.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Animation))]
+public class AnimationShower : Editor
+{
+    private int _curIndex = 0;
+    private List<string> animsName = new List<string>();
+    public void Awake()
+    {
+        UpdateAnimations();
+    }
+
+    public override void OnInspectorGUI ()
+    {
+        EditorGUI.BeginChangeCheck();
+        base.OnInspectorGUI();
+        if (EditorGUI.EndChangeCheck())
+        {
+            UpdateAnimations();
+        }
+        Animation anim = target as Animation;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("动画: ");
+        _curIndex = EditorGUILayout.Popup(_curIndex, animsName.ToArray());
+        if (GUILayout.Button("播放"))
+        {
+            if (_curIndex < animsName.Count)
+            {
+                anim.Stop();
+                anim.Play(animsName[_curIndex]);
+            }
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    protected void UpdateAnimations()
+    {
+        Animation anim = target as Animation;
+        var enumerator = anim.GetEnumerator();
+        while(enumerator.MoveNext())
+        {
+            AnimationState state = enumerator.Current as AnimationState;
+            if(state.clip != null) animsName.Add(state.clip.name);
+        }
+    }
+}
